9a03af9f448471798b6a8b860ba78a118c1dea38
Douglas Maria <dougsm@gmail.com>
2021-11-18 01:38:20 -0300

    cadastrando regiões

--- server.ts
+++ server.ts
@@ -1,13 +1,17 @@
 import "reflect-metadata";
 import express from 'express';
 import {createConnection} from "typeorm";
+import { router } from "./src/routes";
 
 const connection = createConnection();
 
 const app = express();
 
-app.get('/teste',(req,resp)=>{
-  return resp.send('oi')
-})
+// app.get('/teste',(req,resp)=>{
+//   return resp.send('oi')
+// })
+
+app.use(express.json())
+app.use(router)
 
 app.listen(3000,() => console.log('Server is up http://localhost:3000'))
